ae508f3dda3e84a3d03f3c193d5bff26e6c2972c
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-02-10 18:29:59 +0200

    Add static assets

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const path = require('path');
+
 const initHandlebars = require('./config/handlebars');
 
 const app = express();
 initHandlebars(app);
 
+app.use(express.static(path.resolve(__dirname, './public')));
+
 app.all('/', (req, res) => {
-    res.render('index', { layout: false });
+    res.render('index');
 });
 
 app.listen(3000, console.log.bind(console, 'Application is running on http://localhost:3000'));
